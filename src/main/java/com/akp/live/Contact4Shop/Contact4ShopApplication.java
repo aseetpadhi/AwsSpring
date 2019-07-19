c2a4bfdb8159badda76f30069f5c56b1883b1865
aseetpadhi11 <aseetpadhi11@gmail.com>
2019-07-20 00:44:09 +0530

    first commit

---
+++ src/main/java/com/akp/live/Contact4Shop/Contact4ShopApplication.java
@@ -0,0 +1,13 @@
+package com.akp.live.Contact4Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Contact4ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Contact4ShopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/akp/live/Contact4Shop/HelloShop.java
@@ -0,0 +1,14 @@
+package com.akp.live.Contact4Shop;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloShop {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "Hello AWS Spring";
+	}
+
+}
