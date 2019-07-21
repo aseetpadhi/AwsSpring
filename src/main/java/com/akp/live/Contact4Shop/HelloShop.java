2ee8315d3965c75557149fac728ecb5b97126985
aseetpadhi11 <aseetpadhi11@gmail.com>
2019-07-21 22:11:16 +0530

    added shops api

--- src/main/java/com/akp/live/Contact4Shop/HelloShop.java
+++ src/main/java/com/akp/live/Contact4Shop/HelloShop.java
@@ -1,14 +1,42 @@
 package com.akp.live.Contact4Shop;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloShop {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
 	@RequestMapping("/")
 	public String sayHello() {
 		return "Hello AWS Spring -";
+	}
+	
+	@GetMapping("/shops")
+	public ResponseEntity<String> getShops() {
+		final String api = "https://jsonplaceholder.typicode.com/todos/1";
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		return restTemplate.exchange(api, HttpMethod.GET, entity, String.class);
 	}
 
 }
